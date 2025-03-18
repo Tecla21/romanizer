8b9007da9d79509e1b7d9c2fca19de83a9c84643
muatsoft <amuatelembe@gmail.com>
2025-03-18 09:10:29 +0100

    create project

---
+++ spec/romanizer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Romanizer do
+  it "has a version number" do
+    expect(Romanizer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
